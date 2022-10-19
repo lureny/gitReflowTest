00f2d450b0b109e250e4184a3e8e12501312d133
yantao <yantao@smartlink.com.cn>
2022-10-19 16:08:22 +0800

    🔧工具(custom): 提交规范工具

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat",
-        "upd",
-        "del",
-        "fix",
-        "refactor",
-        "test",
-        "perf",
-        "docs",
-        "style",
-        "revert",
-      ],
-    ],
-    "subject-full-stop": [0, "never"],
-    "subject-case": [0, "never"],
-  },
+  // extends: ["@commitlint/config-conventional"], // 原始的changeLog规范
+  extends: ["cz"], // 启用自定义规范
+  // rules: {
+  //   "type-enum": [
+  //     2,
+  //     "always",
+  //     [
+  //       "feat",
+  //       "upd",
+  //       "del",
+  //       "fix",
+  //       "refactor",
+  //       "test",
+  //       "perf",
+  //       "docs",
+  //       "style",
+  //       "revert",
+  //     ],
+  //   ],
+  //   "subject-full-stop": [0, "never"],
+  //   "subject-case": [0, "never"],
+  // },
 };
